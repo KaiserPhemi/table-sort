f2c1b2e0bb5e3657b4c9b621354efb1b440d8224
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-07-09 14:32:11 +0100

    chore(init)

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+// dependencies
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackMd5Hash = require("webpack-md5-hash");
+
+// main configuration
+module.exports = {
+  mode: process.env.NODE_ENV === "development" ? "development" : "production",
+  entry: { app: "./src/Index.tsx" },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  devtool: process.env.NODE_ENV === "development" ? "source-map" : false,
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Writ App",
+      template: "./src/index.html",
+      favicon: "assets/images/favicon.ico",
+      filename: "index.html",
+      hash: true,
+      minify: true,
+    }),
+    new WebpackMd5Hash(),
+  ],
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      {
+        test: /\.scss$/,
+        loader: ["style-loader", "css-loader", "sass-loader"],
+      },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+    ],
+  },
+};
